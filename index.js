00478ae1777047690a46e5b7e693ff327a8cbca6
Wesley Scholl <wscholl@totalwine.com>
2023-09-05 15:53:29 -0400

    Added fib func
    Co-authored-by: konjoinfinity <wesleyscholl@gmail.com>

--- index.js
+++ index.js
@@ -16,3 +16,41 @@ function factorial(n) {
   console.log(factorialOf5); // 120
 
 // This code will print the factorial of 5, which is 120.
+
+
+function fibonacci(n) {
+    if (n === 0) {
+      return 0;
+    } else if (n === 1) {
+      return 1;
+    } else {
+      return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+  }
+//   Use code with caution. Learn more
+//   This function calculates the Fibonacci number for a given number. The Fibonacci numbers are a sequence of numbers where each number is the sum of the two previous numbers. For example, the first 10 Fibonacci numbers are:
+  
+//   0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+  
+//   This function works by using recursion. The Fibonacci function calls itself recursively to calculate the Fibonacci number for the two previous numbers. The base cases of the recursion are when the number is 0 or 1, in which case the function returns the number itself.
+  
+//   Here is an example of how to use this function:
+  
+//   JavaScript
+  const fibonacciOf5 = fibonacci(5);
+  console.log(fibonacciOf5); // 5
+ 
+//   This code will print the Fibonacci number of 5, which is 5.
+  
+//   I hope this helps!
+  
+//   The Fibonacci function is a classic example of a recursive function. It is also a very efficient function, as it only needs to calculate the Fibonacci numbers for the two previous numbers. This makes it a good choice for calculating Fibonacci numbers for large numbers.
+  
+//   Here are some other complex JavaScript functions:
+  
+//   The quicksort algorithm is a sorting algorithm that is very efficient for sorting large lists of data.
+//   The binary search algorithm is a search algorithm that can be used to find an element in a sorted list of data.
+//   The regular expression engine is a powerful tool that can be used to match patterns in text.
+//   The DOM API is a set of functions that can be used to interact with the document object model.
+//   The Node.js API is a set of functions that can be used to build web applications and server-side applications.
+//   These are just a few examples of complex JavaScript functions. There are many other complex functions that can be written in JavaScript, depending on the specific needs of the application.
